1484e825f55f3a4a225383131eff16f3ca8780ea
mirandacaceres2008-eng <mirandacaceres2008@gmail.com>
2026-04-10 11:45:45 -0500

    funciones creadas

---
+++ app.js
@@ -0,0 +1,23 @@
+const aprendices = [ { nombre: "Ana", nota: 4.5, programa: "ADSO" }, 
+     { nombre: "Luis",nota: 2.8, programa: "ADSO" },
+      { nombre: "Marta", nota: 3.7, programa: "Diseno Web" }, 
+      {nombre: "Pedro", nota: 1.9, programa: "ADSO" }, 
+      { nombre: "Sofia", nota: 5.0, programa:"Diseno Web" } ];
+
+function mostrarAprendices () {
+    console.log("lista de aprendices");
+
+    aprendices.forEach(a => {
+        console.log(a.nombre + " -" +  a.nota + " -" + a.programa);
+    });
+} 
+
+function mostrarAprobados() {
+    const aprobados = aprendices.filter(a => a.nota >= 3);
+
+    console.log("aprendices aprobados", aprobados);
+ aprobados.forEach(a => {
+    console.log(a.nombre + "-" + a.nota);
+ });
+}
+
